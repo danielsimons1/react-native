ac1379702c29bd49b55b945d5ebba27659e9919e
Daniel Simons <daniel.simons1@gmail.com>
2021-04-11 23:04:59 +0100

    Update import

--- android/src/main/java/com/pspdfkit/views/ReactPdfUiFragment.java
+++ android/src/main/java/com/pspdfkit/views/ReactPdfUiFragment.java
@@ -24,7 +24,7 @@ import androidx.fragment.app.FragmentManager;
 import com.pspdfkit.react.R;
 import com.pspdfkit.ui.PdfFragment;
 import com.pspdfkit.ui.PdfUiFragment;
-import com.pspdfkit.ui.PdfDocument;
+import com.pspdfkit.document.PdfDocument;
 
 /**
  * This {@link PdfUiFragment} provides additional callbacks to improve integration into react native.
