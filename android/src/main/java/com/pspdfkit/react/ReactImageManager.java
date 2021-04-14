4628ecf07eda8171c48172f3a5685b1d2f1c51f0
Daniel Simons <daniel.simons1@gmail.com>
2021-04-13 23:04:04 -0500

    add createViewInstance

--- android/src/main/java/com/pspdfkit/react/ReactImageManager.java
+++ android/src/main/java/com/pspdfkit/react/ReactImageManager.java
@@ -1,7 +1,9 @@
 
 import com.facebook.react.uimanager.SimpleViewManager;
+import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.views.image.ReactImageView;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.drawee.backends.pipeline.Fresco;
 
 public class ReactImageManager extends SimpleViewManager<ReactImageView> {
 
@@ -10,6 +12,11 @@ public class ReactImageManager extends SimpleViewManager<ReactImageView> {
 
   public ReactImageManager(ReactApplicationContext reactContext) {
     mCallerContext = reactContext;
+  }
+
+  @Override
+  public ReactImageView createViewInstance(ThemedReactContext context) {
+    return new ReactImageView(context, Fresco.newDraweeControllerBuilder(), null, mCallerContext);
   }
 
   @Override
