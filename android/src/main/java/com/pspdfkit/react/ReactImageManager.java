6837514b84626ddc48a636912f7c28aa5ef1cdf8
Daniel Simons <daniel.simons1@gmail.com>
2021-04-14 22:25:49 -0500

    add ReactImageManager to viewManagers

--- android/src/main/java/com/pspdfkit/react/ReactImageManager.java
+++ android/src/main/java/com/pspdfkit/react/ReactImageManager.java
@@ -1,4 +1,5 @@
 
+import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.views.image.ReactImageView;
