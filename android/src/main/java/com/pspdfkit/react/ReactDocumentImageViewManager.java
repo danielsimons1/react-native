03e92b71d1b01521c3b34eab342474c6bd3ca874
Daniel Simons <daniel.simons1@gmail.com>
2021-05-16 15:01:33 -0500

    Add back React Document Image Viewer

---
+++ android/src/main/java/com/pspdfkit/react/ReactDocumentImageViewManager.java
@@ -0,0 +1,37 @@
+
+package com.pspdfkit.react;
+
+import com.facebook.react.uimanager.annotations.ReactProp;
+import com.facebook.react.uimanager.SimpleViewManager;
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.views.image.ReactImageView;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+import androidx.annotation.Nullable;
+
+import com.facebook.react.bridge.ReadableArray;
+
+public class ReactDocumentImageViewManager extends SimpleViewManager<ReactImageView> {
+
+    ReactApplicationContext mCallerContext;
+
+    public ReactDocumentImageViewManager(ReactApplicationContext reactContext) {
+        mCallerContext = reactContext;
+    }
+
+    @Override
+    public ReactImageView createViewInstance(ThemedReactContext context) {
+        return new ReactImageView(context, Fresco.newDraweeControllerBuilder(), null, mCallerContext);
+    }
+
+    @ReactProp(name = "src")
+    public void setSrc(ReactImageView view, @Nullable ReadableArray sources) {
+        view.setSource(sources);
+    }
+
+    @Override
+    public String getName() {
+        return "RCTDocumentImageView";
+    }
+}
