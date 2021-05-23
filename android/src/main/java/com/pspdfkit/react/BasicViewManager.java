85c8902dadf8334cba797ce37eae9077b7c68e53
Daniel Simons <daniel.simons1@gmail.com>
2021-05-23 14:32:53 -0500

    BasicView

--- android/src/main/java/com/pspdfkit/react/BasicViewManager.java
+++ android/src/main/java/com/pspdfkit/react/BasicViewManager.java
@@ -8,6 +8,7 @@ import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.views.image.ReactImageView;
+import androidx.annotation.Nullable;
 
 public class BasicViewManager extends SimpleViewManager<View> {
     ReactApplicationContext mCallerContext;
