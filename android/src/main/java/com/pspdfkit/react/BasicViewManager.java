0309a7b2797c2b51b8ee8f17e0c761a8df9a0678
Daniel Simons <daniel.simons1@gmail.com>
2021-05-23 14:30:46 -0500

    BasicView

--- android/src/main/java/com/pspdfkit/react/BasicViewManager.java
+++ android/src/main/java/com/pspdfkit/react/BasicViewManager.java
@@ -1,15 +1,13 @@
 package main.java.com.pspdfkit.react;
 
-import android.util.Log;
+import android.graphics.Color;
 import android.view.View;
-import com.facebook.react.uimanager.annotations.ReactProp;
+
+import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
-import com.facebook.react.views.image.ImageResizeMode;
+import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.views.image.ReactImageView;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.drawee.backends.pipeline.Fresco;
-import com.pspdfkit.views.PdfView;
 
 public class BasicViewManager extends SimpleViewManager<View> {
     ReactApplicationContext mCallerContext;
