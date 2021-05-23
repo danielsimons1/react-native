662bca4a1eb7e12385a36f32176ab921c66135f8
Daniel Simons <daniel.simons1@gmail.com>
2021-05-23 10:09:06 -0500

    fix index.js

--- android/src/main/java/com/pspdfkit/react/ReactDocumentImageViewManager.java
+++ android/src/main/java/com/pspdfkit/react/ReactDocumentImageViewManager.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.views.image.ImageResizeMode;
 import com.facebook.react.views.image.ReactImageView;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.drawee.backends.pipeline.Fresco;
