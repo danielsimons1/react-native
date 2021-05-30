8a0d4c6b925fc2ec383e35aa42897f20e35c41b9
Daniel Simons <daniel.simons1@gmail.com>
2021-05-30 12:11:55 -0500

    Bump Version

--- android/src/main/java/com/pspdfkit/views/PdfReactImageView.java
+++ android/src/main/java/com/pspdfkit/views/PdfReactImageView.java
@@ -1,5 +1,7 @@
 package main.java.com.pspdfkit.views;
 
+import com.facebook.react.views.image.ReactImageView;
+
 public class PdfReactImageView extends ReactImageView {
     public int pageIndex = 0;
 
